0eb3a098ebd2a3487d59b7426056e3d8054d80dd
kamal <63578559+kamal-debug@users.noreply.github.com>
2021-02-04 13:38:51 +0530

    Add files via upload

---
+++ Server.c
@@ -0,0 +1,30 @@
+#include <unistd.h> 
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <stdlib.h> 
+#include <netinet/in.h> 
+#include <string.h> 
+#define PORT 8080 
+int main(int argc, char const *argv[]) 
+{ 
+	int server_fd, new_socket, valread; 
+	struct sockaddr_in address; 
+	int opt = 1; 
+	int addrlen = sizeof(address); 
+	char buffer[1024] = {0}; 
+	
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
+	{ 
+		perror("socket failed"); 
+		exit(EXIT_FAILURE); 
+	} 
+	
+	
+	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,	&opt, sizeof(opt))) 
+	{ 
+		perror("setsockopt"); 
+		exit(EXIT_FAILURE); 
+	} 
+	address.sin_family = AF_INET; 
+	address.sin_addr.s_addr = INADDR_ANY; 
+	addr…
---
+++ Temperature_Thruster.c
@@ -0,0 +1,68 @@
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <unistd.h> 
+#include <string.h> 
+#include <time.h> 
+#define PORT 8080 
+
+struct packet{
+
+    int id;
+    int packno;
+    int data;
+    time_t time; // opaque data type
+
+}tcs_packet;
+
+int Randfunc(int lower, int upper, int count) 
+{ 
+    int i,num; 
+    for (i = 0; i < count; i++)
+    { 
+         num = (rand() % (upper - lower + 1)) + lower; 
+    } 
+    return num;
+} 
+int main(int argc, char const *argv[])
+{
+    int lower = -100, upper = +100, count = 1; 
+    srand(time(0)); 
+
+    tcs_packet.data = Randfunc(lower, upper, count);
+    printf("Data=%d",tcs_packet.data);
+
+    tcs_packet.id=110;
+    printf("Packet Id=%d\n",tcs_packet.id);
+
+    tcs_packet.packno=10;
+    printf("Packet Id=%d\n",tcs_packet.packno);
+
+
+    int sockfd, connfd; 
+    struct sockaddr_in servaddr, cli; 
+    
+    sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+    if (sockfd == -1) { 
+        printf("socket creation failed...\n"); 
+        exit(0); 
+    } 
+    else
+    {
+        printf("Socket successfully created..\n"); 
+    }
+    // Assign IP,Port
+    servaddr.sin_family = AF_INET; 
+    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1"); 
+    servaddr.sin_port = htons(PORT);
+
+    if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0)
+    { 
+        printf("connection with the server failed...\n"); 
+        exit(0); 
+    } 
+    else
+        printf("connected to the server..\n"); 
+  
+    return 0; 
+}
